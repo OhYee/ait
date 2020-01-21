e456656bbf0d047da6bbda92d5c3eeed13ef3e52
OhYee <oyohyee@oyohyee.com>
2020-01-22 00:03:42 +0800

    fix: response uninitial success field(#master)

--- message/response.go
+++ message/response.go
@@ -14,6 +14,7 @@ type Response struct {
 // NewResponse initial a Response
 func NewResponse(success bool, args []byte) *Response {
 	return &Response{
+		Success:   success,
 		Arguments: args,
 	}
 }
